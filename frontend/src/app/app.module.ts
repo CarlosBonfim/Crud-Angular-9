cfeb7910cb3ce86300d9d9000d58dbe8c2fde256
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2021-07-05 19:20:45 -0400

    comentario

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,6 +13,6 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] //aponta pra o componente que será carregado primeiro
 })
 export class AppModule { }
