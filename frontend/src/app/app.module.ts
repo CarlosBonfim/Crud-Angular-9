1697fba8a203e57ed7cfa3498a5d261e8c003065
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2021-07-12 10:31:00 -0400

    Imports e Identação
    Importar FormsModule, MatInputModule, MatFormFieldModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,21 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component'
 import { RedDirective } from './directives/red.directive'
 
-import {MatSidenavModule} from '@angular/material/sidenav'
-import {MatCardModule} from '@angular/material/card'
-import {MatButtonModule} from '@angular/material/button'
-import {MatListModule} from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
+import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/products/product-create/product-create.component';
 // import { ForDirective } from './directives/for.directive';
-import {MatSnackBarModule} from '@angular/material/snack-bar'
+import { MatSnackBarModule } from '@angular/material/snack-bar'
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
     declarations: [
@@ -31,7 +34,7 @@ import { HttpClientModule } from "@angular/common/http";
         ProductCrudComponent,
         RedDirective,
         ProductCreateComponent,
-        
+
         // ForDirective
     ],
     imports: [
@@ -44,8 +47,11 @@ import { HttpClientModule } from "@angular/common/http";
         MatCardModule,
         MatButtonModule,
         MatSnackBarModule,
-        HttpClientModule
-        
+        HttpClientModule,
+        FormsModule,
+        MatInputModule,
+        MatFormFieldModule
+
     ],
     providers: [],
     bootstrap: [AppComponent] //aponta pra o componente que será carregado primeiro
